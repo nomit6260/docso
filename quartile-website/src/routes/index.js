0a06b10a80513762614d9e3d453fe0f4b7242a9b
NomitQuartiles <80898370+nomit6260@users.noreply.github.com>
2021-03-28 12:41:21 +0530

    updated index

--- quartile-website/src/routes/index.js
+++ quartile-website/src/routes/index.js
@@ -7,6 +7,13 @@ import Navbar from "components/Navbar/Navbar"
 import Footer from "components/Footer/Footer"
 
 const HomeComponent = lazy(() => import('components/Home/HomeComponent'));
+const WhyUsComponent = lazy(() => import('components/Pages/WhyUsComponent'));
+const ContactUsComponent = lazy(() => import('components/Pages/ContactUsComponent'));
+const TeamComponent = lazy(() => import('components/Pages/TeamComponent'));
+const CareerComponent = lazy(() => import('components/Pages/CareerComponent'));
+const JobApplicationComponent = lazy(() => import('components/Pages/JobApplicationComponent'));
+const JobdescComponent = lazy(() => import('components/Pages/JobdescComponent'));
+const AboutUsComponent = lazy(() => import('components/Pages/AboutUsComponent'));
 
 export const Root = ({ theme }) => (
   <BrowserRouter>
@@ -14,6 +21,13 @@ export const Root = ({ theme }) => (
     <Suspense fallback={<LoadingComponent loading />}>
       <Switch>
         <Route exact path={SLUGS.home} component={HomeComponent} />
+        <Route exact path={SLUGS.whyus} component={WhyUsComponent} />
+        <Route exact path={SLUGS.contactus} component={ContactUsComponent} />
+        <Route exact path={SLUGS.team} component={TeamComponent} />
+        <Route exact path={SLUGS.careers} component={CareerComponent} />
+        <Route exact path={SLUGS.jobapplication} component={JobApplicationComponent} />
+        <Route exact path={SLUGS.jobdesc} component={JobdescComponent} />
+        <Route exact path={SLUGS.aboutus} component={AboutUsComponent} />
         <Redirect to={SLUGS.home} />
       </Switch>
     </Suspense>
