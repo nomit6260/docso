815d4ab3e711eacee84784b11dc53fdf052ff32a
nomit6260 <nomit.kasera@quartilelabs.com>
2021-04-23 23:48:43 +0530

    UPDATE

--- docso-website/src/components/Home/HomeComponent.js
+++ docso-website/src/components/Home/HomeComponent.js
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 import "animate.css/animate.min.css";
 import ScrollAnimation from 'react-animate-on-scroll';
 import Carousel from "components/Carousel/CarouselNauka"
-
+import AdvantageComponent from "components/Pages/AdvantageComponent"
 import Img1 from "components/ScreenShot/home.png"
 import Img2 from "components/ScreenShot/drawer.png"
 import Img3 from "components/ScreenShot/book.png"
@@ -139,6 +139,8 @@ function HomeComponent() {
             <br/><br/>
             <br/>
             <br/>
+
+            <AdvantageComponent/>
             
         </Container>
     )
