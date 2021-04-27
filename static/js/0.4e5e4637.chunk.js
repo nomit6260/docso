07574520b70c334d2b21c7a5b66cae194c907412
nomit6260 <nomit.kasera@quartilelabs.com>
2021-04-27 19:12:39 +0530

    Updates

--- static/js/0.4e5e4637.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpquartile-website"]=this["webpackJsonpquartile-website"]||[]).push([[0],{42:function(e,i,s){}}]);
-//# sourceMappingURL=0.4e5e4637.chunk.js.map
---
+++ static/js/0.d58564c2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpquartile-website"]=this["webpackJsonpquartile-website"]||[]).push([[0],{43:function(e,i,s){}}]);
+//# sourceMappingURL=0.d58564c2.chunk.js.map
